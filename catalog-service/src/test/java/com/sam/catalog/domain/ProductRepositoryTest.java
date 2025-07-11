dffb0a00e30dffd68521e1899553b7a6c13a2883
Sammy Kumar <sammykumardemo@gmail.com>
2025-07-11 15:47:18 +0530

    configuration updated 7

--- catalog-service/src/test/java/com/sam/catalog/domain/ProductRepositoryTest.java
+++ catalog-service/src/test/java/com/sam/catalog/domain/ProductRepositoryTest.java
@@ -7,6 +7,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 /*@DataJpaTest(
